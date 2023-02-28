a94768bbdf8be1ebce8a73e22bd9e2a1fdbc2ae4
San Rosch <san.rosch@htl.rennweg.at>
2023-02-28 20:54:38 +0100

    Class Knight new

--- scripts/pieceClasses/knight.js
+++ scripts/pieceClasses/knight.js
@@ -2,15 +2,16 @@ class Knight extends Piece {
   constructor(pos, color) {
     super(
       pos,
-      color[
-        ("2Right/1Up",
+      color,
+      [
+        "2Right/1Up",
         "1Right/2Up",
         "1Left/2Up",
         "2Left/1Up",
         "2Right/1Down",
         "1Right/2Down",
         "1Left/2Down",
-        "2Left/1Down")
+        "2Left/1Down",
       ],
       "knight"
     );
