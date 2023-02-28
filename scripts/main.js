d0c59acf40a0f91ae3995eb98b7a755d43639f4e
Clemens Wolfsberger <1058@htl.rennweg.at>
2023-02-28 20:18:19 +0100

    ChessboardLayout

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+for(let i = 0; i < 64; i++){
+    let div = document.createElement("div");
+    
+
+    if(i % 16 < 8){
+        if(i % 2 == 0){
+            div.style.backgroundColor = "rgb(235,235,211)";
+        }else{
+            div.style.backgroundColor = "rgb(116,148,84)";
+        }
+    }else{
+        if(i % 2 != 0){
+            div.style.backgroundColor = "rgb(235,235,211)";
+        }else{
+            div.style.backgroundColor = "rgb(116,148,84)";
+        }
+    }
+
+    document.getElementById("chessboard").appendChild(div);
+}
