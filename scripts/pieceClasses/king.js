d863164ef5bd01aa19812ad334102bc06ceda930
San Rosch <san.rosch@htl.rennweg.at>
2023-02-28 21:03:58 +0100

    King

--- scripts/pieceClasses/king.js
+++ scripts/pieceClasses/king.js
@@ -1,5 +1,5 @@
 class King extends Piece{
     constructor(pos,color){
-        super(pos,color,["1up","1Down","1Left","1Right","1Left/1Up","1Right/1Up","1Left/1Down","1Right/1Down"],"King")
+        super(pos,color,["1Up","1Down","1Left","1Right","1Left/1Up","1Right/1Up","1Left/1Down","1Right/1Down"],"King")
     }
 }
