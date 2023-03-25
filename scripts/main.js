932490f8ae92bc07239ac0af1f4cb97933665a28
San Rosch <san.rosch@htl.rennweg.at>
2023-03-25 19:55:29 +0100

    getTile

--- scripts/main.js
+++ scripts/main.js
@@ -17,10 +17,6 @@ let pieces = [
   new Knight("78", "White"),
   new Rook("88", "White"),
 
-
-
-
-
   new Pawn("12", "Black"),
   new Pawn("22", "Black"),
   new Pawn("32", "Black"),
@@ -36,43 +32,67 @@ let pieces = [
   new King("51,", "Black"),
   new Bishop("61", "Black"),
   new Knight("71", "Black"),
-  new Rook("81", "Black")
+  new Rook("81", "Black"),
 ];
+let chessboard=document.getElementById("chessboard");
+chessboard.addEventListener("contextmenu", drawArrow);
+
+drawboard();
+drawPieces();
+
+function drawArrow(event) {
+  event.preventDefault();
+  
+  console.log(getTile(event).row);
+}
 
 
-function drawPieces(){
-  for(let i = 0; i < pieces.length; i++){
+
+function getTile(event){
+  let column=(event.clientX - chessboard.getBoundingClientRect().left)/100;
+  let row=(event.clientY - chessboard.getBoundingClientRect().top)/100;
+  return{column:Math.ceil(column), row: Math.ceil(row) };
+}
+
+//Draw
+
+function drawPieces() {
+  for (let i = 0; i < pieces.length; i++) {
     let fig = document.createElement("div");
     fig.className = "figure";
-    fig.style.backgroundImage = "url('../img/" + pieces[i].name + pieces[i].color + ".png')";
+    fig.style.backgroundImage =
+      "url('../img/" + pieces[i].name + pieces[i].color + ".png')";
 
     fig.style.gridColumn = parseInt(pieces[i].pos.charAt(0));
     fig.style.gridRow = parseInt(pieces[i].pos.charAt(1));
-    document.getElementById("figures").appendChild(fig)
+    document.getElementById("figures").appendChild(fig);
   }
 }
 
-drawPieces();
+function drawboard() {
+  //Draw Chessboard Tiles
+  for (let i = 0; i < 64; i++) {
+    let div = document.createElement("div");
+    
 
-//Draw Chessboard Tiles
-for (let i = 0; i < 64; i++) {
-  let div = document.createElement("div");
-
-  if (i % 16 < 8) {
-    if (i % 2 == 0) {
-      div.style.backgroundColor = "rgb(235,235,211)";
+    if (i % 16 < 8) {
+      if (i % 2 == 0) {
+        div.style.backgroundColor = "rgb(235,235,211)";
+      } else {
+        div.style.backgroundColor = "rgb(116,148,84)";
+      }
     } else {
-      div.style.backgroundColor = "rgb(116,148,84)";
+      if (i % 2 != 0) {
+        div.style.backgroundColor = "rgb(235,235,211)";
+      } else {
+        div.style.backgroundColor = "rgb(116,148,84)";
+      }
     }
-  } else {
-    if (i % 2 != 0) {
-      div.style.backgroundColor = "rgb(235,235,211)";
-    } else {
-      div.style.backgroundColor = "rgb(116,148,84)";
-    }
+    
+    chessboard.appendChild(div);
   }
-
-  document.getElementById("chessboard").appendChild(div);
 }
+
+
 
 const d = new Pawn("01", "Black");
